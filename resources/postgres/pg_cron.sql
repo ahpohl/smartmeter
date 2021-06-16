cbead5ed2ba3484e99c660c3e75ea23dc95b0e1d
Alexander Pohl <alex@ahpohl.com>
2021-06-16 19:54:22 +0200

    rename pg_cron jobs

--- resources/postgres/pg_cron.sql
+++ resources/postgres/pg_cron.sql
@@ -8,11 +8,11 @@ SELECT cron.schedule('purge_log', '13 0 * * *', $$DELETE
     WHERE end_time < now() - INTERVAL '3 days'$$);
 
 -- schedule updates
-SELECT cron.schedule('daily_view', '0,15,30,45 * * * *', $$REFRESH
+SELECT cron.schedule('smartmeter_daily', '0,15,30,45 * * * *', $$REFRESH
   MATERIALIZED VIEW CONCURRENTLY daily_view$$);
 
-SELECT cron.schedule('monthly_view', '1,16,31,46 * * * *', $$REFRESH
+SELECT cron.schedule('smartmeter_monthly', '1,16,31,46 * * * *', $$REFRESH
   MATERIALIZED VIEW CONCURRENTLY monthly_view$$);
 
 UPDATE cron.job SET database = 'smartmeter' WHERE 
-  jobname = 'daily_view' OR jobname = 'monthly_view';
+  jobname = 'smartmeter_daily' OR jobname = 'smartmeter_monthly';
