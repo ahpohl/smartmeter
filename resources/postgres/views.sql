e8fd13bed0312bf7c3c2ab4b12be526ed7907145
Alexander Pohl <alex@ahpohl.com>
2022-02-02 22:15:23 +0100

    insert comment in views.sql

--- resources/postgres/views.sql
+++ resources/postgres/views.sql
@@ -23,6 +23,7 @@ SELECT
   price,
   rate
 FROM cagg_daily JOIN plan ON cagg_daily.plan_id = plan.id
+-- insert end time of archive
 WHERE bucket_1d > TIMESTAMP WITH TIME ZONE '2022-02-01 01:00:00+01'
 GROUP BY bucket_1d, energy_1d, total, price, rate
 ORDER BY time;
