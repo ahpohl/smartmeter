1f790d919503f0808eb8584693f7116384f51f1b
Alexander Pohl <alex@ahpohl.com>
2024-02-18 21:25:57 +0100

    add 15 min aggregate (load profile)

--- resources/postgres/aggregates.sql
+++ resources/postgres/aggregates.sql
@@ -53,3 +53,30 @@ SELECT add_retention_policy('cagg_power', INTERVAL '1 year');
 
 -- grant
 GRANT SELECT ON TABLE cagg_power TO grafana;
+
+--
+-- load profile for Fronius Solar.creator
+--
+DROP MATERIALIZED VIEW IF EXISTS cagg_minutes CASCADE;
+
+CREATE MATERIALIZED VIEW cagg_minutes
+WITH (timescaledb.continuous, timescaledb.materialized_only=true) 
+AS
+SELECT
+  time_bucket('15 minutes', time, 'Europe/Berlin') AS bucket_15m,
+  sensor_id,
+  plan_id,
+  last(energy, time) - first(energy, time) AS energy_15m,
+  first(energy, time) AS total
+FROM live
+GROUP BY bucket_15m, plan_id, sensor_id
+WITH NO DATA;
+
+-- refresh policy
+SELECT add_continuous_aggregate_policy('cagg_minutes',
+  start_offset => INTERVAL '3 hours',
+  end_offset => INTERVAL '15 minutes',
+  schedule_interval => '15 minutes');
+
+-- data retention
+SELECT add_retention_policy('cagg_minutes', INTERVAL '10 years');
